1426f9da15bb2754554cac5248593221dd738c18
AnsBalin <ansbalin@gmail.com>
2016-03-03 09:46:56 +0000

    null commit

---
+++ src/anal/vmdscript.tcl
@@ -0,0 +1,113 @@
+mol new /home/balin/Desktop/helix/exp1/R_260_271.xyz
+mol new /home/balin/Desktop/helix/exp1/R_260_272.xyz
+mol new /home/balin/Desktop/helix/exp1/R_260_273.xyz
+mol new /home/balin/Desktop/helix/exp1/R_260_274.xyz
+mol new /home/balin/Desktop/helix/exp1/R_260_275.xyz
+mol new /home/balin/Desktop/helix/exp1/R_260_276.xyz
+mol new /home/balin/Desktop/helix/exp1/R_260_277.xyz
+mol new /home/balin/Desktop/helix/exp1/R_260_278.xyz
+mol new /home/balin/Desktop/helix/exp1/R_260_279.xyz
+mol new /home/balin/Desktop/helix/exp1/R_260_280.xyz 
+
+
+mol delrep 0 top
+mol color Name
+mol representation Lines
+mol selection {atomicnumber 0}
+mol addrep 0
+
+
+mol color Molecule
+mol representation VDW 0.6
+mol selection {atomicnumber 1}
+mol addrep 0
+
+# ONE
+
+mol delrep 0 1
+
+mol color Molecule
+mol representation VDW 0.6
+mol selection {atomicnumber 1}
+mol addrep 1
+
+
+# TWO
+
+mol delrep 0 2
+
+mol color Molecule
+mol representation VDW 0.6
+mol selection {atomicnumber 1}
+mol addrep 2
+
+
+# 3
+
+mol delrep 0 3
+
+mol color Molecule
+mol representation VDW 0.6
+mol selection {atomicnumber 1}
+mol addrep 3
+
+
+# 4
+
+mol delrep 0 4
+
+mol color Molecule
+mol representation VDW 0.6
+mol selection {atomicnumber 1}
+mol addrep 4
+
+
+# 5
+
+mol delrep 0 5
+
+mol color Molecule
+mol representation VDW 0.6
+mol selection {atomicnumber 1}
+mol addrep 5
+
+
+# 6
+
+mol delrep 0 6
+
+mol color Molecule
+mol representation VDW 0.6
+mol selection {atomicnumber 1}
+mol addrep 6
+
+
+# 7
+
+mol delrep 0 7
+
+mol color Molecule
+mol representation VDW 0.6
+mol selection {atomicnumber 1}
+mol addrep 7
+
+
+# 8
+
+mol delrep 0 8
+
+mol color Molecule
+mol representation VDW 0.6
+mol selection {atomicnumber 1}
+mol addrep 8
+
+
+# 9
+
+mol delrep 0 9
+
+mol color Molecule
+mol representation VDW 0.6
+mol selection {atomicnumber 1}
+mol addrep 9
+
